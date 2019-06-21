917fe9b6f0474e9c50469283e02a43a4da130480
Cho <yijeong@f01898a3fa41.ant.amazon.com>
2019-06-21 15:50:28 +0900

    shell update

--- beer-supplychain-fabric-client/env_setting.sh
+++ beer-supplychain-fabric-client/env_setting.sh
@@ -17,7 +17,7 @@ fi
 echo Creating a keypair named $NETWORKNAME-keypair. The .pem file will be in your $HOME directory
 aws ec2 create-key-pair --key-name $NETWORKNAME-keypair --region $REGION --query 'KeyMaterial' --output text > ~/$NETWORKNAME-keypair.pem
 if [ $? -gt 0 ]; then
-    echo Keypair $NETWORKNAME-keypair could not be created
+    echo Keypair $NETWORKNAME-keypair could not be created. Please delete the old one. 
     exit $?
 fi
 
--- cloud9_env_setting.sh
+++ cloud9_env_setting.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# This bash shell script set environment variables for cloud9 IDE 
+
+
 export REGION=us-east-1 
 export NETWORKID= #<your blockchain network ID>
 export NETWORKNAME=beer
